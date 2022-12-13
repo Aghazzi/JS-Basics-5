ce243bc32bdb316246b03edfcbf297d9324c2963
Aghazzi <aghazzi.1998@gmail.com>
2022-12-13 16:41:37 +0200

    condition

---
+++ EX2/ex2.js
@@ -0,0 +1,5 @@
+document.getElementById("validate").onclick = () => {
+     name=document.getElementById("name").value 
+     name1=document.getElementById("name1").value
+    alert ("Hello, " + name + "\n" + "Hello, " + name1)
+}
